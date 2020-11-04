8bcb90588cd4b0eddac76e1ea5166864aefa460d
Thilo Behnke <thilo.behnke@gmx.net>
2020-11-04 17:36:38 +0100

    feature/server-boilerplate

---
+++ server/src/test/groovy/server/ServerSpec.groovy
@@ -0,0 +1,19 @@
+package server
+
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.annotation.MicronautTest
+import spock.lang.Specification
+import javax.inject.Inject
+
+@MicronautTest
+class ServerSpec extends Specification {
+
+    @Inject
+    EmbeddedApplication application
+
+    void 'test it works'() {
+        expect:
+        application.running
+    }
+
+}
