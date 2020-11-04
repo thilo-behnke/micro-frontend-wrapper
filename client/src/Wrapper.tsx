4fafb03d6615702c1be3dbb203be59792a3fc611
Thilo Behnke <thilo.behnke@gmx.net>
2020-11-05 00:32:00 +0100

    feature/loading-spinner-for-header

--- client/src/Wrapper.tsx
+++ client/src/Wrapper.tsx
@@ -1,49 +1,68 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import "./Wrapper.scss";
 import {
-    AppManifest,
-    DefaultManifestProvider,
-    ManifestProvider,
-    ManifestProviderContext
+  AppManifest,
+  DefaultManifestProvider,
+  ManifestProvider,
+  ManifestProviderContext,
 } from "./manifest/ManifestProvider";
 import Content from "./content/Content";
 import Header from "./header/Header";
-import {usePrevious} from "./utils/PreviousHook";
+import { usePrevious } from "./utils/PreviousHook";
 
 // TODO: There needs to be a wrapper around App that provides this value.
 const manifestProvider: ManifestProvider = new DefaultManifestProvider();
 
 function Wrapper() {
-    const [activeApp, setActiveApp] = useState<AppManifest | null>(null);
-    const [availableApps, setAvailableApps] = useState<AppManifest[]>([]);
+  const [isLoading, setLoading] = useState<boolean>(false);
+  const [activeApp, setActiveApp] = useState<AppManifest | null>(null);
+  const [availableApps, setAvailableApps] = useState<AppManifest[]>([]);
 
-    const prevActiveApp = usePrevious<AppManifest | null>(activeApp) as AppManifest | null;
+  const prevActiveApp = usePrevious<AppManifest | null>(
+    activeApp
+  ) as AppManifest | null;
 
-    useEffect(() => {
-        const subscription = manifestProvider.loadApps()
-            .subscribe((apps: AppManifest[]) => {
-                setAvailableApps(apps);
-                apps.forEach(({url}) => {
-                    const script = document.createElement('script');
-                    script.src = url
-                    script.async = true
-                    document.body.append(script);
-                })
-            });
-        return () => subscription.unsubscribe();
-    }, [])
+  useEffect(() => {
+    setLoading(true);
+    const subscription = manifestProvider
+      .loadApps()
+      .subscribe((apps: AppManifest[]) => {
+        setAvailableApps(apps);
+        apps.forEach(({ url }) => {
+          const script = document.createElement("script");
+          script.src = url;
+          script.async = true;
+          document.body.append(script);
+        });
+        setLoading(false);
+      });
+    return () => subscription.unsubscribe();
+  }, []);
 
-    const activateApp = (container: HTMLElement, appManifest: AppManifest | null) => {
-        manifestProvider.switchApps(container, prevActiveApp, appManifest);
-    };
+  const activateApp = (
+    container: HTMLElement,
+    appManifest: AppManifest | null
+  ) => {
+    manifestProvider.switchApps(container, prevActiveApp, appManifest);
+  };
 
-    return (
+  return (
     <div className="app">
-        <ManifestProviderContext.Provider value={manifestProvider}>
-            <Header className="app__header" availableApps={availableApps} activeApp={activeApp} setActiveApp={setActiveApp}/>
-            <Content activeApp={activeApp} activateApp={activateApp} className="app__content"/>
-            <div className="app__footer">footer</div>
-        </ManifestProviderContext.Provider>
+      <ManifestProviderContext.Provider value={manifestProvider}>
+        <Header
+          className="app__header"
+          isLoading={isLoading}
+          availableApps={availableApps}
+          activeApp={activeApp}
+          setActiveApp={setActiveApp}
+        />
+        <Content
+          activeApp={activeApp}
+          activateApp={activateApp}
+          className="app__content"
+        />
+        <div className="app__footer">footer</div>
+      </ManifestProviderContext.Provider>
     </div>
   );
 }
--- client/src/header/Header.tsx
+++ client/src/header/Header.tsx
@@ -1,19 +1,40 @@
 import React from "react";
-import {AppManifest} from "../manifest/ManifestProvider";
-import {Button} from "react-bootstrap";
+import { AppManifest } from "../manifest/ManifestProvider";
+import { Button, Spinner } from "react-bootstrap";
 import classNames from "classnames";
-import './Header.scss';
+import "./Header.scss";
 
 function Header(props: HeaderProps) {
-    return <div className={classNames(props.className, 'header-content')}> {props.availableApps.map(app => <Button variant={app.appId === props.activeApp?.appId && app.version === props.activeApp?.version ? 'primary' : 'secondary'} onClick={() => props.setActiveApp(app)}
-        key={app.appId}>{app.appName}</Button>)} </div>
+  return (
+    <div className={classNames(props.className, "header-content")}>
+      {props.isLoading ? (
+        <Spinner animation="border" />
+      ) : (
+        props.availableApps.map((app) => (
+          <Button
+            variant={
+              app.appId === props.activeApp?.appId &&
+              app.version === props.activeApp?.version
+                ? "primary"
+                : "secondary"
+            }
+            onClick={() => props.setActiveApp(app)}
+            key={app.appId}
+          >
+            {app.appName}
+          </Button>
+        ))
+      )}
+    </div>
+  );
 }
 
 export type HeaderProps = {
-    activeApp: AppManifest | null;
-    availableApps: AppManifest[];
-    setActiveApp: (app: AppManifest) => void;
-    className?: string;
-}
+  isLoading: boolean;
+  activeApp: AppManifest | null;
+  availableApps: AppManifest[];
+  setActiveApp: (app: AppManifest) => void;
+  className?: string;
+};
 
 export default Header;
