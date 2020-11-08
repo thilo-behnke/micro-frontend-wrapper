4b8e891e92dea255f2332ada2ed48d2fe92a8fb6
Thilo Behnke <thilo.behnke@gmx.net>
2020-11-08 11:38:02 +0100

    adding debug info

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,10 +9,12 @@ echo "Deploying onto server $SERVER_URL with user $SERVER_USER"
 
 scp .env-production $SERVER_USER@$SERVER_URL:/home/build/
 if [ $? -eq 0 ]; then
+    echo "ERROR on copying env file"
     exit $?
 fi
 
 ssh $SERVER_USER@$SERVER_URL DOCKER_REGISTRY=$DOCKER_REGISTRY 'bash -s' < scripts/redeploy-containers.sh
 if [ $? -eq 0 ]; then
+    echo "ERROR on redeploying containers"
     exit $?
 fi
