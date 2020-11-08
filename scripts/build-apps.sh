fdc655b2c5e44156e653bcadad1d32e04c3c51c6
Thilo Behnke <thilo.behnke@gmx.net>
2020-11-08 19:03:37 +0100

    add todos

--- scripts/build-apps.sh
+++ scripts/build-apps.sh
@@ -6,3 +6,6 @@ cd micro-frontends/product-search-client
 npm install
 npm run build
 aws s3 cp public/build/product-search-app__*.js s3://micro-frontend-app-assets
+# TODO: Insert version into app-manifest.json - not possible with rollup, just use gulp?
+# TODO: Use manifest data to register frontend at ManifestController.
+# TODO: How to handle the s3 url? Public get for the beginning I guess.
