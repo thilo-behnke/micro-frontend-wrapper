a7c22b68599567ab68ea498fccc9cd9b5e988897
Thilo Behnke <thilo.behnke@gmx.net>
2020-11-08 11:17:27 +0100

    add docker registry env variable to ssh script access

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,8 +3,9 @@
 set -u
 : "$SERVER_URL"
 : "$SERVER_USER"
+: "$DOCKER_REGISTRY"
 
 echo "Deploying onto server $SERVER_URL with user $SERVER_USER"
 
 scp .env-production $SERVER_USER@$SERVER_URL:/home/build/
-ssh $SERVER_USER@$SERVER_URL 'bash -s' < scripts/redeploy-containers.sh
+ssh $SERVER_USER@$SERVER_URL DOCKER_REGISTRY=$DOCKER_REGISTRY 'bash -s' < scripts/redeploy-containers.sh
