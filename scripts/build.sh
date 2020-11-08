56da5b14cee0bb15a819aeec921627391561e6da
Thilo Behnke <thilo.behnke@gmx.net>
2020-11-08 11:58:30 +0100

    use default mongo image instead of container image (has to change)

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,7 +6,7 @@ set -u
 
 echo "Using docker registry: $DOCKER_REGISTRY"
 
-export $(cat .env-production | xargs) && docker-compose build client server service-registry service-registry-mongo
+export $(cat .env-production | xargs) && docker-compose build client server service-registry
 
 docker tag microfrontend_server $DOCKER_REGISTRY/micro_frontend_wrapper_server:latest
 docker tag microfrontend_server $DOCKER_REGISTRY/micro_frontend_wrapper_server:$BUILD_NUMBER
@@ -16,8 +16,8 @@ docker tag microfrontend_service-registry $DOCKER_REGISTRY/micro_frontend_wrappe
 docker tag microfrontend_service-registry $DOCKER_REGISTRY/micro_frontend_wrapper_service_registry:$BUILD_NUMBER
 docker push $DOCKER_REGISTRY/micro_frontend_wrapper_service_registry
 
-docker tag microfrontend_service-registry-mongo $DOCKER_REGISTRY/micro_frontend_wrapper_service_registry_mongo:latest
-docker tag microfrontend_service-registry-mongo $DOCKER_REGISTRY/micro_frontend_wrapper_service_registry_mongo:$BUILD_NUMBER
+docker tag mongo:latest $DOCKER_REGISTRY/micro_frontend_wrapper_service_registry_mongo:latest
+docker tag mongo:latest $DOCKER_REGISTRY/micro_frontend_wrapper_service_registry_mongo:$BUILD_NUMBER
 docker push $DOCKER_REGISTRY/micro_frontend_wrapper_service_registry_mongo
 
 docker tag microfrontend_client $DOCKER_REGISTRY/micro_frontend_wrapper_client:latest
