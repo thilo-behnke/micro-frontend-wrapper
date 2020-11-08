51aa4081eb9460076bfa55f18bba7dee3b2e6ac9
Thilo Behnke <thilo.behnke@gmx.net>
2020-11-08 11:35:46 +0100

    exit if ssh script fails

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,4 +8,9 @@ set -u
 echo "Deploying onto server $SERVER_URL with user $SERVER_USER"
 
 scp .env-production $SERVER_USER@$SERVER_URL:/home/build/
+if [ $? -eq 0 ]; then
+    exit $?
+
 ssh $SERVER_USER@$SERVER_URL DOCKER_REGISTRY=$DOCKER_REGISTRY 'bash -s' < scripts/redeploy-containers.sh
+if [ $? -eq 0 ]; then
+    exit $?
