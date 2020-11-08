d565f62a62361e3a6281c8f0a6292adcabe02d7e
Thilo Behnke <thilo.behnke@gmx.net>
2020-11-08 12:14:38 +0100

    devOps/move-deployment-process-to-scripts

---
+++ scripts/build.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+: "$DOCKER_REGISTRY"
+
+echo "Using docker registry: $DOCKER_REGISTRY"
+
+export $(cat .env-production | xargs) && docker-compose build client server service-registry service-registry-mongo
+
+docker tag microfrontend_server $DOCKER_REGISTRY/micro_frontend_wrapper_server:latest
+docker tag microfrontend_server $DOCKER_REGISTRY/micro_frontend_wrapper_server:$BUILD_NUMBER
+docker push $DOCKER_REGISTRY/micro_frontend_wrapper_server
+
+docker tag microfrontend_service-registry $DOCKER_REGISTRY/micro_frontend_wrapper_service_registry:latest
+docker tag microfrontend_service-registry $DOCKER_REGISTRY/micro_frontend_wrapper_service_registry:$BUILD_NUMBER
+docker push $DOCKER_REGISTRY/micro_frontend_wrapper_service_registry
+
+docker tag microfrontend_service-registry-mongo $DOCKER_REGISTRY/micro_frontend_wrapper_service_registry_mongo:latest
+docker tag microfrontend_service-registry-mongo $DOCKER_REGISTRY/micro_frontend_wrapper_service_registry_mongo:$BUILD_NUMBER
+docker push $DOCKER_REGISTRY/micro_frontend_wrapper_service_registry_mongo
+
+docker tag microfrontend_client $DOCKER_REGISTRY/micro_frontend_wrapper_client:latest
+docker tag microfrontend_client $DOCKER_REGISTRY/micro_frontend_wrapper_client:$BUILD_NUMBER
+docker push $DOCKER_REGISTRY/micro_frontend_wrapper_client
---
+++ scripts/deploy.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+: "$SERVER_URL"
+: "$SERVER_USER"
+: "$DOCKER_REGISTRY"
+
+echo "Deploying onto server $SERVER_URL with user $SERVER_USER"
+
+scp .env-production $SERVER_USER@$SERVER_URL:/home/build/
+if [ $? -eq 0 ]; then
+    echo "Successfully copied environment file to server."
+  else
+    echo "Exit code: $?"
+    echo "ERROR on copying env file"
+    exit $?
+fi
+
+ssh $SERVER_USER@$SERVER_URL DOCKER_REGISTRY=$DOCKER_REGISTRY 'bash -s' < scripts/redeploy-containers.sh
+if [ $? -eq 0 ]; then
+    echo "Successfully redeployed containers."
+  else
+    echo "Exit code: $?"
+    echo "ERROR on redeploying containers"
+    exit $?
+fi
