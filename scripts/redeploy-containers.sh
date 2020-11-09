6bcb5a73e04fa0f58de48689b965563d1ee1b817
Thilo Behnke <thilo.behnke@gmx.net>
2020-11-10 00:07:40 +0100

    remove cleanup

--- scripts/redeploy-containers.sh
+++ scripts/redeploy-containers.sh
@@ -16,16 +16,12 @@ docker run -d --env-file .env-production --name server --network "app-net" $DOCK
 
 docker pull $DOCKER_REGISTRY/micro_frontend_wrapper_server_mongo:latest
 docker run -d --env-file .env-production --name server_mongo --network "app-net" $DOCKER_REGISTRY/micro_frontend_wrapper_server_mongo:latest
-docker rmi $DOCKER_REGISTRY/micro_frontend_wrapper_server_mongo:latest
 
 docker pull $DOCKER_REGISTRY/micro_frontend_wrapper_service_registry:latest
 docker run -d --env-file .env-production --name service_registry --network "app-net" $DOCKER_REGISTRY/micro_frontend_wrapper_service_registry:latest
-docker rmi $DOCKER_REGISTRY/micro_frontend_wrapper_service_registry:latest
 
 docker pull $DOCKER_REGISTRY/micro_frontend_wrapper_service_registry_mongo:latest
 docker run -d --env-file .env-production --name service_registry_mongo --network "app-net" $DOCKER_REGISTRY/micro_frontend_wrapper_service_registry_mongo:latest
-docker rmi $DOCKER_REGISTRY/micro_frontend_wrapper_service_registry_mongo:latest
 
 docker pull $DOCKER_REGISTRY/micro_frontend_wrapper_client:latest
 docker run -d --env-file .env-production --name client --network "app-net" -p "80:80" $DOCKER_REGISTRY/micro_frontend_wrapper_client:latest
-docker rmi $DOCKER_REGISTRY/micro_frontend_wrapper_client:latest
