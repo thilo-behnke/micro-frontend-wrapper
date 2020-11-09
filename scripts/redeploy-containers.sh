82456fe0ffdb24c8a26130cdde8399cc9f1af41b
Thilo Behnke <thilo.behnke@gmx.net>
2020-11-10 00:04:04 +0100

    perform more cleanup

--- scripts/redeploy-containers.sh
+++ scripts/redeploy-containers.sh
@@ -7,19 +7,25 @@ set -u
 echo "Using docker registry: $DOCKER_REGISTRY"
 
 (docker stop server && docker rm server && false) || (docker stop server_mongo && docker rm server_mongo && false) || (docker stop service_registry && docker rm service_registry && false) || (docker stop service_registry_mongo && docker rm service_registry_mongo && false) || (docker stop client && docker rm client && false) || true
+docker system prune -f
 
 docker network create 'app-net' || true
 
 docker pull $DOCKER_REGISTRY/micro_frontend_wrapper_server:latest
-docker pull $DOCKER_REGISTRY/micro_frontend_wrapper_server_mongo:latest
-docker pull $DOCKER_REGISTRY/micro_frontend_wrapper_service_registry:latest
-docker pull $DOCKER_REGISTRY/micro_frontend_wrapper_service_registry_mongo:latest
-docker pull $DOCKER_REGISTRY/micro_frontend_wrapper_client:latest
-
 docker run -d --env-file .env-production --name server --network "app-net" $DOCKER_REGISTRY/micro_frontend_wrapper_server:latest
-docker run -d --env-file .env-production --name server_mongo --network "app-net" $DOCKER_REGISTRY/micro_frontend_wrapper_server_mongo:latest
-docker run -d --env-file .env-production --name service_registry --network "app-net" $DOCKER_REGISTRY/micro_frontend_wrapper_service_registry:latest
-docker run -d --env-file .env-production --name service_registry_mongo --network "app-net" $DOCKER_REGISTRY/micro_frontend_wrapper_service_registry_mongo:latest
-docker run -d --env-file .env-production --name client --network "app-net" -p "80:80" $DOCKER_REGISTRY/micro_frontend_wrapper_client:latest
 
-docker system prune -f
+docker pull $DOCKER_REGISTRY/micro_frontend_wrapper_server_mongo:latest
+docker run -d --env-file .env-production --name server_mongo --network "app-net" $DOCKER_REGISTRY/micro_frontend_wrapper_server_mongo:latest
+docker rmi $DOCKER_REGISTRY/micro_frontend_wrapper_server_mongo:latest
+
+docker pull $DOCKER_REGISTRY/micro_frontend_wrapper_service_registry:latest
+docker run -d --env-file .env-production --name service_registry --network "app-net" $DOCKER_REGISTRY/micro_frontend_wrapper_service_registry:latest
+docker rmi $DOCKER_REGISTRY/micro_frontend_wrapper_service_registry:latest
+
+docker pull $DOCKER_REGISTRY/micro_frontend_wrapper_service_registry_mongo:latest
+docker run -d --env-file .env-production --name service_registry_mongo --network "app-net" $DOCKER_REGISTRY/micro_frontend_wrapper_service_registry_mongo:latest
+docker rmi $DOCKER_REGISTRY/micro_frontend_wrapper_service_registry_mongo:latest
+
+docker pull $DOCKER_REGISTRY/micro_frontend_wrapper_client:latest
+docker run -d --env-file .env-production --name client --network "app-net" -p "80:80" $DOCKER_REGISTRY/micro_frontend_wrapper_client:latest
+docker rmi $DOCKER_REGISTRY/micro_frontend_wrapper_client:latest
