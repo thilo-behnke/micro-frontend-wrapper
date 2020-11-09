d541aa4476e46d05880c663ca49ba95ec1fbb186
Thilo Behnke <thilo.behnke@gmx.net>
2020-11-09 20:12:30 +0100

    write deployment script for svelte micro frontend

--- scripts/build-apps.sh
+++ scripts/build-apps.sh
@@ -1,11 +1,32 @@
 #!/usr/bin/env bash
 
 set -e
+set -u
+: "$APP_REGISTRY_URL"
+: "$S3_URL"
+
+echo "Using app registry with url $APP_REGISTRY_URL."
+echo "Using s3 storage with url $S3_URL."
+
+# Register micro frontend.
 
 cd micro-frontends/product-search-client
 npm install
 npm run build
-aws s3 cp public/build/product-search-app__*.js s3://micro-frontend-app-assets
-# TODO: Insert version into app-manifest.json - not possible with rollup, just use gulp?
-# TODO: Use manifest data to register frontend at ManifestController.
-# TODO: How to handle the s3 url? Public get for the beginning I guess.
+
+app_version=$(jq -r '.appVersion' public/build/app-manifest.json)
+js_bundle_filename=public/build/product-search-app__$app_version.js
+app_url="$S3_URL/micro-frontend-app-assets"
+json=$(jq -r --arg app_url $app_url '. + {appUrl: $app_url}' public/build/app-manifest.json)
+
+aws s3 cp js_bundle_filename s3://micro-frontend-app-assets
+curl -X POST -H "Content-Type: application/json" -d json $APP_REGISTRY_URL/manifest-api/manifests
+
+# Register micro frontend bff.
+
+cd ../product-search-backend
+
+# TODO: Build as docker container.
+# TODO: Push docker container into registry
+# TODO: Register at service registry
+
