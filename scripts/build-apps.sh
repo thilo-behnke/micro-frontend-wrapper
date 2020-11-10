adb11ee54807cc2c9db8758c27b153fc04505bad
Thilo Behnke <thilo.behnke@gmx.net>
2020-11-10 21:12:33 +0100

    dont expand json

--- scripts/build-apps.sh
+++ scripts/build-apps.sh
@@ -25,7 +25,7 @@ app_url="$S3_URL/$js_bundle_filename"
 json=$(jq -r --arg app_url $app_url '. + {appUrl: $app_url}' public/build/app-manifest.json)
 
 aws s3 cp "$js_bundle_filename" s3://micro-frontend-app-assets
-curl -X POST -H "Content-Type: application/json" -d $json $APP_REGISTRY_URL/manifest-api/manifests
+curl -X POST -H "Content-Type: application/json" -d "$json" $APP_REGISTRY_URL/manifest-api/manifests
 
 # Deploy and register micro frontend bff.
 
