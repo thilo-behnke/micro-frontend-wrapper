5c63c7a940905f480a54f51bf3e0fe18aa0bca35
Thilo Behnke <thilo.behnke@gmx.net>
2020-11-24 23:29:47 +0100

    devOps/properly-set-build-arg

--- scripts/build-apps.sh
+++ scripts/build-apps.sh
@@ -31,8 +31,7 @@ curl -X POST -H "Content-Type: application/json" -d "$json" $APP_REGISTRY_URL/ma
 
 cd ../product-search-backend
 
-export MICRONAUT_ENVIRONMENTS=prod
-docker build -t $DOCKER_REGISTRY/micro_frontend_wrapper_product_search_backend:$BUILD_NUMBER -t $DOCKER_REGISTRY/micro_frontend_wrapper_product_search_backend:latest .
+docker build -t $DOCKER_REGISTRY/micro_frontend_wrapper_product_search_backend:$BUILD_NUMBER -t $DOCKER_REGISTRY/micro_frontend_wrapper_product_search_backend:latest --build-arg MICRONAUT_ENVIRONMENTS=prod .
 docker push $DOCKER_REGISTRY/micro_frontend_wrapper_product_search_backend
 
 ssh $PRODUCT_SEARCH_API_SERVER_USER@$PRODUCT_SEARCH_API_SERVER_URL "docker stop product_search_backend && docker rm product_search_backend || true"
