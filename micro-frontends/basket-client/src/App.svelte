9bab9ce968229b00d631b0c84b34e00a0e765614
Thilo Behnke <thilo.behnke@gmx.net>
2020-12-12 16:54:18 +0100

    basket client implementation

--- micro-frontends/basket-client/src/App.svelte
+++ micro-frontends/basket-client/src/App.svelte
@@ -13,106 +13,36 @@
 </style>
 
 <script lang="ts">
-  import SvelteTable from "svelte-table";
-  import dayjs from "dayjs";
-  import BasketActions from "./BasketActions.svelte";
-  import type { Product } from "./model/product";
-  import type {
-    EventHandler,
-    IncrementBasketCount,
-  } from "./model/micro-frontend";
-  import { EventType } from "./model/micro-frontend";
+    import type {EventHandler,} from "./model/micro-frontend";
+    import {EventType} from "./model/micro-frontend";
+    import type {Basket} from "./model/basket";
 
-  export let services;
+    export let services;
   export let eventHandler: EventHandler;
 
-  const onIncrementBasket = (product: Product) => {
-    eventHandler.send<Product>({
-      type: EventType.INCREMENT_BASKET_COUNT,
-      payload: product,
-    });
-  };
+  let basket: Basket;
 
-  const onDecrementBasket = (product: Product) => {
-    eventHandler.send<Product>({
-      type: EventType.DECREMENT_BASKET_COUNT,
-      payload: product,
-    });
-  };
-
-  let products: Product[] = [];
-  let searchText;
-
-  $: filteredProducts = searchText
-    ? products.filter(
-        ({ name, desc }) =>
-          name.toLowerCase().includes(searchText.toLowerCase()) ||
-          desc.toLowerCase().includes(searchText.toLowerCase())
-      )
-    : products;
-
-  const columns = [
-    {
-      key: "name",
-      title: "Product Name",
-      value: (v) => v.name,
-      sortable: true,
-    },
-    {
-      key: "desc",
-      title: "Description",
-      value: (v) => (v.desc.length < 40 ? v.desc : v.desc.slice(0, 40) + "..."),
-    },
-    {
-      key: "price",
-      title: "Price in $",
-      value: (v) => v.price,
-      sortable: true,
-    },
-    {
-      key: "release",
-      title: "Release Date",
-      value: (v) => dayjs(v.release).format("DD-MM-YYYY"),
-      sortable: true,
-    },
-    {
-      key: "actions",
-      title: "",
-      renderComponent: {
-        component: BasketActions,
-        props: {
-          onIncrementBasket,
-          onDecrementBasket,
-        },
-      },
-    },
-  ];
-
-  const productSearchServiceUrl = services.find(
-    ({ serviceId }) => serviceId === "product-search-api"
+  const basketServiceUrl = services.find(
+    ({ serviceId }) => serviceId === "basket-api"
   )?.serviceUrl;
-  if (!productSearchServiceUrl) {
+  if (!basketServiceUrl) {
     throw new Error("Cant find any registered service for product search api.");
   }
-  fetch(productSearchServiceUrl + "/api/products")
+  fetch(basketServiceUrl + "/api/basket")
     .then((res) => res.json())
-    .then((resProducts) => (products = resProducts));
+    .then((resBasket) => (basket = resBasket));
 
   eventHandler
     .receive(EventType.INCREMENT_BASKET_COUNT, EventType.DECREMENT_BASKET_COUNT)
     .subscribe(console.log);
 </script>
 
-<!--TODO: Implement add to basket button.-->
 <main class="wrapper">
-  {#if products?.length}
-    <input
-      class="product-table__search"
-      placeholder="search products by name or description"
-      bind:value={searchText} />
-    <SvelteTable
-      {columns}
-      rows={filteredProducts}
-      classNameTable="table table-dark" />
+  {#if basket}
+    <ul>
+    {#each basket.items as item}
+        <li>{item.product.name}</li>
+    {/each}
+    </ul>
   {:else}loading...{/if}
 </main>
---
+++ micro-frontends/basket-client/src/model/basket.ts
@@ -0,0 +1,10 @@
+import type {Product} from "./product";
+
+export type BasketItem = {
+    product: Product;
+    count: number;
+}
+
+export type Basket = {
+    items: BasketItem[]
+}
